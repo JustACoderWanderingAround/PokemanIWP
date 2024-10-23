b82cf25324aa42ad567b6e098599dd176cc1264b
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-10-23 15:45:09 +0800

    added sample code

--- Assets/Script/SceneSwitcher.cs
+++ Assets/Script/SceneSwitcher.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static void SwapScene(string nextScene) 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene(nextScene);
     }
 }
