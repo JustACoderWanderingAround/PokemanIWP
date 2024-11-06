3ba57fd4bcd0f7372f7ffae3a0ac543f9fcae592
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-11-06 19:11:18 +0800

    simple ground enemy now moves on its own, can now idle too. added zombie

--- Assets/Script/AI/NavAgentMovementController.cs
+++ Assets/Script/AI/NavAgentMovementController.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class NavAgentMovementController : MonoBehaviour
 {
+    NavMeshAgent m_agent;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        m_agent = GetComponent<NavMeshAgent>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetTarget(Vector3 newTarget)
     {
-        
+        m_agent.destination = newTarget;
+    }
+    public NavMeshAgent GetAgent() => m_agent;
+
+    public void StopNavigation()
+    {
+        m_agent.speed = 0;
+        m_agent.isStopped = true;
+    }
+    public void ResumeNavigation()
+    {
+        m_agent.isStopped = false;
+    }
+    // Pick the next point iwthin the room to move to
+    // Function taken from https://www.youtube.com/watch?v=dYs0WRzzoRc
+    public bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, UnityEngine.AI.NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        {
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 }
--- Assets/Script/AI/SimpleGroundEnemy.cs
+++ Assets/Script/AI/SimpleGroundEnemy.cs
@@ -1,33 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SimpleGroundEnemy : DamagableEntity
 {
     [SerializeField]
-    NavAgentMovementController m_MovementController;
+    NavAgentMovementController movementController;
+    [SerializeField]
+    private float nextTargetMaxRange = 5.0f;
+    NavMeshAgent agent;
+    FieldOfView fov;
 
     // AI Related vars
-
     float stateTimer;
+    Vector3 nextTarget;
     enum EnemyState
     {
         STATE_IDLE,
         STATE_PATROL,
         STATE_ATTACK
     }
+    EnemyState state;
 
     protected override void Awake()
     {
         base.Awake();
+        agent = movementController.GetAgent();
+        state = EnemyState.STATE_PATROL;
+        Vector3 point;
+        movementController.RandomPoint(transform.position, nextTargetMaxRange, out point);
+        movementController.GetAgent().destination = point;
     }
 
     // plan: randomly walk around
     // after a few seconds, idle
     // keep walking
 
+    // Update is called once per frame
     void Update()
     {
-        // 
+        // TODO: Player distance check logic + sound check logic
+        UpdateStateMachine();
+    }
+    void UpdateStateMachine()
+    {
+        stateTimer += Time.deltaTime;
+        switch (state)
+        {
+            case EnemyState.STATE_IDLE:
+                if (stateTimer > 5.0f)
+                {
+                    movementController.SetTarget(nextTarget);
+                    ChangeState(EnemyState.STATE_PATROL);
+                }
+                break;
+            case EnemyState.STATE_PATROL:
+                if (agent.remainingDistance <= agent.stoppingDistance) //done with path
+                {
+                    Vector3 point;
+                    if (movementController.RandomPoint(transform.position, Random.Range(0, nextTargetMaxRange), out point)) //pass in our centre point and radius of area
+                    {
+                        ChangeState(EnemyState.STATE_IDLE);
+                        nextTarget = point;
+                    }
+                }
+                break;
+            case EnemyState.STATE_ATTACK:
+                break;
+        }
+    }
+    void ChangeState(EnemyState newState)
+    {
+        Debug.Log("State changed");
+        stateTimer = 0;
+        state = newState;
     }
 }
