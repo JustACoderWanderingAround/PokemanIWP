97f270e26bd767bb1dfc82829c2499ccdea9da80
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-11-25 11:50:21 +0800

    add zombie sounds

---
+++ Assets/Script/AI/EntityRandomSoundPlayer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class EntityRandomSoundPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    List<AudioClip> availableClips = new List<AudioClip>();
+    [SerializeField]
+    float maxSoundTimer = 15.0f;
+    float soundTimer;
+    AudioSource audioSource;
+    [SerializeField]
+    bool playOnAwake = false;
+
+    private void Awake()
+    {
+
+        if (!playOnAwake)
+            soundTimer = maxSoundTimer; 
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        soundTimer -= Time.deltaTime;
+        if (soundTimer < 0)
+        {
+            soundTimer = maxSoundTimer;
+            audioSource.clip = availableClips[Random.Range(0, availableClips.Count)];
+            audioSource.Play();
+        }
+    }
+}
