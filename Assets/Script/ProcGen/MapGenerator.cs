8336abfe365e74131aa3da75daf3f30dc0bc5238
JustACoderWanderingAround <pgbaquiran@gmail.com>
2024-12-09 18:07:07 +0800

    can now spawn start and end tiles

--- Assets/Script/ProcGen/MapGenerator.cs
+++ Assets/Script/ProcGen/MapGenerator.cs
@@ -10,32 +10,92 @@ public class MapGenerator : MonoBehaviour
     [SerializeField]
     float startY = -22.5f;
     [SerializeField]
-    float tileSize = 5;
+    float tileSizeX = 5;
+    [SerializeField]
+    float tileSizeY = 5;
     [SerializeField]
     int mapSeed = 0;
     [SerializeField]
     int mapSizeX = 10;
     [SerializeField]
     int mapSizeY = 10;
-
-    // TODO: update with tile system
-    [Header("Map Prefabs")]
     [SerializeField]
-    List<GameObject> mapPrefabs;
+    float mapSpawnExitMinGridDistance = 3;
 
+    [Header("Map prefabs")]
+    [SerializeField]
+    List<MapTileSO> tilesSOs;
 
-    // TODO: tile system then uncomment
-    //List<List<MapTile>> tiles;
+    [SerializeField]
+    MapTileSO spawnTile;
+    [SerializeField]
+    MapTileSO exitTile;
 
-    List<List<GameObject>> tileObjects;
-
+    List<List<MapTileSO>> tileGrid;
+    
     // Start is called before the first frame update
     void Start()
     {
         // init vars
-        List<GameObject> list = new List<GameObject>();
+        tileGrid = new List<List<MapTileSO>>();
+        for (int x = 0; x < mapSizeX; x++)
+        {
+            tileGrid.Add(new List<MapTileSO>());
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                tileGrid[x].Add(new MapTileSO());
+            }
+        }
         Random.InitState(mapSeed);
 
-        // startProcGen
+        GenerateMap();
+
+        SpawnTiles();
+    }
+    void GenerateMap()
+    {
+        // ProcGen Init
+        {
+            // Find random area to place starting tile
+            int startingLocX = Random.Range(0, mapSizeX / 2 );
+            int startingLocY = Random.Range(0, mapSizeY / 2);
+
+            tileGrid[startingLocX][startingLocY] = spawnTile;
+
+            // Find random area to place ending tile 
+
+            int endingLocX = startingLocX;
+            int endingLocY = startingLocY;
+
+           
+
+            do
+            {
+                endingLocX = Random.Range(0, mapSizeX);
+                endingLocY = Random.Range(0, mapSizeY);
+
+            } while (Vector2.Distance(new Vector2(endingLocX, endingLocY), new Vector2(startingLocX, startingLocY)) < mapSpawnExitMinGridDistance);
+            tileGrid[endingLocX][endingLocY] = exitTile;
+            Debug.Log("Start: " + startingLocX.ToString() + " " + startingLocY.ToString());
+            Debug.Log("End: " + endingLocX.ToString() + " " + endingLocY.ToString());
+        }
+        // ProcGen Start
+        {
+            
+        }
+    }
+    void SpawnTiles()
+    {
+       for(int x = 0; x < mapSizeX; x++)
+        {
+            for (int y = 0; y < mapSizeY; y++)
+            {
+                if (tileGrid[x][y].tilePrefab != null)
+                {
+                    Vector3 currentPosition = new Vector3(startX + (mapSizeX * x), 0, startY + (mapSizeY * y));
+                    Instantiate(tileGrid[x][y].tilePrefab, currentPosition, Quaternion.identity);
+                }
+            }
+        }
     }
 }
--- Assets/Script/ScriptableObjects/MapTileSO.cs
+++ Assets/Script/ScriptableObjects/MapTileSO.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[CreateAssetMenu(menuName = "ProcGenMapTile")]
 public class MapTileSO : ScriptableObject
 {
-    [SerializeField]
-    List<MapTileSO> allowedNeighboursXPos;
-    [SerializeField]
-    List<MapTileSO> allowedNeighboursXNeg;
-    [SerializeField]
-    List<MapTileSO> allowedNeighboursYPos;
-    [SerializeField]
-    List<MapTileSO> allowedNeighboursYNeg;
-
+    [Header("0 for blocked, 1 for clear. ")]
+    public List<int> socketIDs = new List<int>() { 1, 1, 1, 1 };
+    public GameObject tilePrefab;
+    [Header("Blocks that cannot connect on a specific socket")]
+    public List<GameObject> disallowedConnectionsXPos;
+    public List<GameObject> disallowedConnectionsYPos; 
+    public List<GameObject> disallowedConnectionsXNeg;
+    public List<GameObject> disallowedConnectionsYNeg;
 }
